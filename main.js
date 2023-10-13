8ee90bff2810b5ee9fcf9bc6df1c4528f2c2bfb1
Santiago Chiaretta <santichiaretta@gmail.com>
2023-10-13 17:47:07 -0300

    correccion de errores generales

--- main.js
+++ main.js
@@ -9,23 +9,22 @@ const productos = [
 ]
 
 let carrito = []
-
 let eleccion = prompt("Hola, bienvenido/a. Desea comprar algún producto?(si/no)")
 
 while (eleccion != "si" && eleccion != "no") {
     alert("Por favor, escriba si o no.")
-    eleccion = prompt("Desea comprar algo?")
+    eleccion = prompt("Desea comprar algo?(si/no)")
 }
 
 if (eleccion == "si" || eleccion == "sI" || eleccion == "SI" || eleccion == "Si") {
     alert("Estos son nuestros productos:")
-    let todoslosProductos = productos.map((producto) => " " + producto.nombre.toUpperCase() + " $" + producto.precio)
-    alert(todoslosProductos)
+    let todoslosProductos = productos.map((producto) => " " + producto.nombre + " $" + producto.precio)
+    alert(todoslosProductos.join(" | "))
 } else if (eleccion == "no" || eleccion == "nO" || eleccion == "NO" || eleccion == "No") {
-    alert("Hasta otra vez será. Gracias")
+    alert("Hasta otra vez será. Gracias!")
 }
 
-while (eleccion != "no" || eleccion != "nO" || eleccion != "NO" || eleccion != "No") {
+while (eleccion == "si" || eleccion == "sI" || eleccion == "SI" || eleccion == "Si") {
     let producto = prompt("Agrega un producto a tu carrito:")
     let precio = 0
     if (producto == "wayfarer" || producto == "aviator" || producto == "ferrari" || producto == "clubmaster" || producto == "round" || producto == "erika" || producto == "evolve") {
@@ -43,7 +42,7 @@ while (eleccion != "no" || eleccion != "nO" || eleccion != "NO" || eleccion != "
                 precio = 73000;
                 break;
             case "round":
-                precio = 77000; 
+                precio = 77000;
                 break;
             case "erika":
                 precio = 75000;
@@ -54,11 +53,18 @@ while (eleccion != "no" || eleccion != "nO" || eleccion != "NO" || eleccion != "
             default:
                 break;
         }
-        let unidades = parseInt(prompt("Cuántas unidades quiere agregar?"))
-
-        carrito.push({producto, unidades, precio})
-        console.log(carrito)
-    }else{
+        let unidades = parseInt(prompt("Cuántas unidades desea agregar?"))
+        carrito.push({ producto, unidades, precio })
+    } else {
         alert(producto.toUpperCase() + " no coincide con ninguno de nuestros modelos")
     }
+
+    eleccion = prompt("Desea seguir comprando?(si/no)")
+    if (eleccion == "no" || eleccion == "nO" || eleccion == "NO" || eleccion == "No"){
+        alert("Gracias por su compra, hasta pronto!")
+    }
 }
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+console.log(carrito)
+console.log(`El total a pagar es: ${total}`)
