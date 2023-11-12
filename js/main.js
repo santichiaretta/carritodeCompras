cf0f38eff27ff85b741a0a3905e90392de0945b3
Santiago Chiaretta <santichiaretta@gmail.com>
2023-11-12 20:43:16 -0300

    entrega proy final

--- js/main.js
+++ js/main.js
@@ -6,50 +6,54 @@ const cartQuantity = document.getElementById("cartQuantity");
 let cart = JSON.parse(localStorage.getItem("Carrito")) || [];
 
 const getProducts = async () => {
-    const response = await fetch("data.json");
-    const data = await response.json();
+    try {
+        const response = await fetch("data.json");
+        const data = await response.json();
 
-    data.forEach((product) => {
-        let content = document.createElement("div");
-        content.className = "card"
-        content.innerHTML = `
+        data.forEach((product) => {
+            let content = document.createElement("div");
+            content.className = "card"
+            content.innerHTML = `
         <img src= "${product.img}">
         <h3>${product.name}</h3>
         <p class="price">${product.price} US$</p>
         `;
 
-        shopContent.append(content);
+            shopContent.append(content);
 
-        let buy = document.createElement("button");
-        buy.innerText = "Comprar";
-        buy.className = "comprar"
+            let buy = document.createElement("button");
+            buy.innerText = "Comprar";
+            buy.className = "comprar"
 
-        content.append(buy);
+            content.append(buy);
 
-        buy.addEventListener("click", () => {
-            const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+            buy.addEventListener("click", () => {
+                const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
 
-            if (repeat) {
-                cart.map((prod) => {
-                    if (prod.id === product.id) {
-                        prod.amount++;
-                    }
-                })
-            } else {
-                cart.push({
-                    id: product.id,
-                    img: product.img,
-                    name: product.name,
-                    price: product.price,
-                    amount: product.amount,
-                });
-            }
-            console.log(cart);
-            console.log(cart.length);
-            cartCounter();
-            saveLocal();
+                if (repeat) {
+                    cart.map((prod) => {
+                        if (prod.id === product.id) {
+                            prod.amount++;
+                        }
+                    })
+                } else {
+                    cart.push({
+                        id: product.id,
+                        img: product.img,
+                        name: product.name,
+                        price: product.price,
+                        amount: product.amount,
+                    });
+                }
+                console.log(cart);
+                console.log(cart.length);
+                cartCounter();
+                saveLocal();
+            });
         });
-    });
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 getProducts();
